093770a11bfa57fa8783b5af81877aa9a098b751
Thiago <gu.thi2@hotmail.com>
2022-08-23 20:49:58 -0300

    feat: orçamento js

--- js/script.js
+++ js/script.js
@@ -12,3 +12,14 @@ function ativarLink(link) {
 links.forEach(ativarLink);
 
 // Ativar itens do orçamento //
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+
+parametros.forEach(ativarProduto);
